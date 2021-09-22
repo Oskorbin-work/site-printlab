0831340754d07a5772653ba834a916e790723d9a
Oskorbin <serhiy.polozyuk@m.hneu.edu.ua>
2021-09-22 09:42:32 +0300

    work with detail service -- add Layout requirements -- add name for img detail -- start work under refactor menu bar day 1

--- service/admin.py
+++ service/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from service.models import (
-Service, Image_service
+Service, Image_service, Service_requirement
 )
 admin.site.register(Service)
 admin.site.register(Image_service)
+admin.site.register(Service_requirement)
 # Register your models here.
--- service/models.py
+++ service/models.py
@@ -15,17 +15,24 @@ class Parameters_service(models.Model):
 """
 
 class Image_service(models.Model):
+    name = models.CharField(max_length=100, default = "Имя картинки")
     img = models.ImageField(
         upload_to='../media_root',
         null=True,
         blank=True,
     )
+
     descriptions = models.TextField()
 
     @property
     def img_url(self):
         if self.img and hasattr(self.img, 'url'):
             return self.img.url
+
+
+class Service_requirement(models.Model):
+    requirement = models.TextField()
+
 
 class Service(models.Model):
     name = models.CharField(
@@ -41,6 +48,8 @@ class Service(models.Model):
     img_service = models.ManyToManyField(Image_service, related_name='img_serv')
 
     description = models.TextField()
+
+    list_requirements = models.ManyToManyField(Service_requirement, related_name='layout_requirements')
 
     def __str__(self):
         return '{}'.format(self.name)
